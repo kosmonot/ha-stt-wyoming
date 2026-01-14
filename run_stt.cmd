b9aef39fc2fe83f3198027094633b7c6f6836555
kosmonot <john@jla3.com>
2026-01-14 03:25:34 -0500

    first commit

---
+++ run_stt.cmd
@@ -0,0 +1,13 @@
+@echo off
+cd /d C:\Users\john\Projects\ha-stt-wyoming
+call C:\Users\john\Projects\ha-stt-wyoming\venv\Scripts\activate.bat
+
+set HF_HUB_DISABLE_SYMLINKS_WARNING=1
+
+python -m wyoming_faster_whisper ^
+  --uri "tcp://0.0.0.0:10300" ^
+  --language en ^
+  --model large ^
+  --device cuda ^
+  --data-dir ".\data" ^
+  --download-dir ".\data"
