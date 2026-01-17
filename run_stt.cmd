9881dc6e18fa8cef3f91477548dbeaa84556f8f3
kosmonot <john@jla3.com>
2026-01-17 02:12:50 -0500

    deleted unneeded symlink error supression

--- run_stt.cmd
+++ run_stt.cmd
@@ -2,8 +2,6 @@
 cd /d C:\Users\john\Projects\ha-stt-wyoming
 call C:\Users\john\Projects\ha-stt-wyoming\venv\Scripts\activate.bat
 
-set HF_HUB_DISABLE_SYMLINKS_WARNING=1
-
 python -m wyoming_faster_whisper ^
   --uri "tcp://0.0.0.0:10300" ^
   --language en ^
